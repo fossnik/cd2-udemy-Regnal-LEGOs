b085df409a39d6ed7c10292e65507a6b8813d1a2
Zack Hartmann <zackhartmann@gmail.com>
2018-01-09 19:31:21 -0500

    Implemented a constructor and instantiator.

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		bullDozer dozer = new bullDozer();
-		dozer.color = "Yellow";
-		dozer.size = "Mini";
+		bullDozer dozer = new bullDozer("Yellow", "Mini");
 
 		System.out.println("Bulldozer is: " + dozer.color);
 	}
@@ -11,6 +9,12 @@ public class Main {
 	public static class bullDozer {
 		private String color;
 		private String size;
+
+		// a public constructor for bullDozer
+		public bullDozer (String _color, String _size) {
+			this.color = _color;
+			this.size = _size;
+		}
 
 		public String getColor() { return color; }
 		public void setColor(String color) { this.color = color; }
