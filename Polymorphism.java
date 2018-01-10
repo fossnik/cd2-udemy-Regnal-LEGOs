b5deca30646df123a74443965a57e439c887c029
Zack Hartmann <zackhartmann@gmail.com>
2018-01-10 10:16:40 -0500

    Added Float Overload Method

--- Polymorphism.java
+++ Polymorphism.java
@@ -4,6 +4,8 @@ public class Polymorphism {
 		FactoryWorker worker = new FactoryWorker();
 		worker.Count(4);
 		worker.Count(5);
+
+		worker.Count(4.5);
 	}
 
 	public static class FactoryWorker {
@@ -16,10 +18,15 @@ public class Polymorphism {
 			System.out.println(Total);
 		}
 
-		// class variable
-		private int Total;
-		public int getTotal() { return Total; }
-		public void setTotal(int total) { this.Total = total; }
+		// overloaded method to accept floating point numbers
+		public void Count(double number) {
+			Total += number;
+			System.out.println(Total);
+		}
 
+		// class variable
+		private float Total;
+		public float getTotal() { return Total; }
+		public void setTotal(int total) { this.Total = total; }
 	}
 }
