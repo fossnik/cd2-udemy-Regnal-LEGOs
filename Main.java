bf7e628311fd9954afe37ab39fc0bc6edf5264b8
Zack Hartmann <zackhartmann@gmail.com>
2018-01-09 19:24:04 -0500

    Implemented a static Class called bullDozer

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello CareerDevs");
-    }
+	public static void main(String[] args) {
+		bullDozer dozer = new bullDozer();
+		dozer.color = "Yellow";
+		dozer.size = "Mini";
+
+		System.out.println("Bulldozer is: " + dozer.color);
+	}
+
+	public static class bullDozer {
+		private String color;
+		private String size;
+
+		public String getColor() { return color; }
+		public void setColor(String color) { this.color = color; }
+
+		public String getSize() { return size; }
+		public void setSize(String size) { this.size = size; }
+	}
+
 }
